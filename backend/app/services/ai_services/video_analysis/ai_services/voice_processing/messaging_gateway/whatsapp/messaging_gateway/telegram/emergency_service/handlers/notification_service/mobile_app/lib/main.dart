6cef65f72a5ce2eaf4347554e3e1b770bf1e52d6
narekArutynan <narekin020@gmail.com>
2025-01-30 16:16:20 +0300

    Create main.dart

---
+++ backend/app/services/ai_services/video_analysis/ai_services/voice_processing/messaging_gateway/whatsapp/messaging_gateway/telegram/emergency_service/handlers/notification_service/mobile_app/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Psychological Support',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Home')),
+      body: Center(child: Text('Welcome to Psychological Support!')),
+    );
+  }
+}
